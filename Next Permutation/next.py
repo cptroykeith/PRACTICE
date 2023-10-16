bfc8023d87a79c6d07f9d22be42b9b959acb8315
your-name <katongole.roy100@gmail.com>
2023-10-16 21:30:09 +0300

    next permutation

---
+++ Next Permutation/next.py
@@ -0,0 +1,30 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = len(nums) - 2
+        while i >= 0 and nums[i] >= nums[i+1]:
+            i -= 1
+        
+        # If no such element exists, reverse the array
+        if i == -1:
+            nums.reverse()
+            return nums
+        
+        # Find the smallest element greater than nums[i] from the right
+        j = len(nums) - 1
+        while j > i and nums[j] <= nums[i]:
+            j -= 1
+        
+        # Swap nums[i] and nums[j]
+        nums[i], nums[j] = nums[j], nums[i]
+        
+        # Reverse the elements to the right of i
+        left, right = i+1, len(nums) - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+        
+        return nums
