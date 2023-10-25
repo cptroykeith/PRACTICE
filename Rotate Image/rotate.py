83ab5959d5d0eeb34031dad3b68bab63f78dbc0a
your-name <katongole.roy100@gmail.com>
2023-10-25 23:33:50 +0300

    rotate image

---
+++ Rotate Image/rotate.py
@@ -0,0 +1,11 @@
+def rotate(matrix):
+    n = len(matrix)
+    
+    # Transpose the matrix (swap rows and columns)
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    
+    # Reverse each row
+    for i in range(n):
+        matrix[i] = matrix[i][::-1]
