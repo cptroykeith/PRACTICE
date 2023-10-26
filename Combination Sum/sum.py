d1df684b0cdadf34c0991733dd1078dad526b3b4
your-name <katongole.roy100@gmail.com>
2023-10-26 22:09:00 +0300

    combination sum

---
+++ Combination Sum/sum.py
@@ -0,0 +1,14 @@
+def combinationSum(candidates, target):
+    def backtrack(start, target, path):
+        if target == 0:
+            result.append(path)
+            return
+        if target < 0:
+            return
+        
+        for i in range(start, len(candidates)):
+            backtrack(i, target - candidates[i], path + [candidates[i]])
+    
+    result = []
+    backtrack(0, target, [])
+    return result
