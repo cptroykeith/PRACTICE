09e9aad10286e1de0a15138e895aa2942e839668
your-name <katongole.roy100@gmail.com>
2023-11-01 21:32:21 +0300

    permutations

---
+++ Permutations II/permutation.py
@@ -0,0 +1,18 @@
+def permuteUnique(nums):
+    def backtrack(start):
+        if start == len(nums):
+            result.append(nums.copy())
+            return
+        
+        used = set()
+        for i in range(start, len(nums)):
+            if nums[i] in used:
+                continue
+            used.add(nums[i])
+            nums[start], nums[i] = nums[i], nums[start]
+            backtrack(start + 1)
+            nums[start], nums[i] = nums[i], nums[start]
+    
+    result = []
+    backtrack(0)
+    return result
